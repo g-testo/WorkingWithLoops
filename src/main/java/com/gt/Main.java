39463766448ea6304f921a018b9805a0f6582ac0
Gregory Testo <gt@gregorytesto.com>
2023-04-19 16:06:33 -0400

    first commit

---
+++ src/main/java/com/gt/Main.java
@@ -0,0 +1,56 @@
+package com.gt;
+
+public class Main {
+    public static void main(String[] args) {
+//        while -
+//        String userInput = "y";
+//
+//        while(userInput.equals("y")){
+//            System.out.println("While");
+//            userInput = "n";
+//        }
+////        do / while
+//        do{
+//            System.out.println("Do, while");
+//        } while(userInput.equals("y"));
+
+
+//        for
+
+//        for( initial value of an iterator, condition for running, increment/decrementing iterator ){}
+//        for(int i=0;i<10;i++){ // 2
+//            System.out.println(i); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//        }
+
+//        for(int i=0;i<10;i+=2){
+//            System.out.println(i); // 0, 2, 4, 6, 8
+//        }
+
+//        for(int i=10;i>0;i--){
+//            System.out.println(i); // 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+//        }
+
+//        for(int i=25;i<100;i++){
+//            System.out.println(i); // 25, 26, 27 ... 99
+//        }
+
+//        for(int i=25;i<=100;i++){
+//            System.out.println(i); // 25, 26, 27 ... 100
+//        }
+
+//        for(int i=0;i<100;i++){
+//            System.out.println(i);
+//            if(i > 15){
+//                break;
+//            }
+//        }
+        for(int i=0;i<100;i++){
+            if(i < 15){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+    }
+}
